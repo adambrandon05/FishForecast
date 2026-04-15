facf6db5c7ba06ebd8ccf40b07863912bb3e6aae
Adam Brandon <brandonm79bb@gmail.com>
2026-04-14 20:44:07 -0600

    little update

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
 require('dotenv').config(); // includes .env data 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits,  } = require('discord.js');
 const TOKEN = process.env.TOKEN; 
 
-const cron = require('node-cron'); 
+// const cron = require('node-cron'); // not currently used but may be used in the future for scheduling messages to be sent at specific times
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages]
 }); 
 
-// logs into discord bot includes a check to prevent errors
-client.once('ready', () => { 
-    console.log(`Logged in as ${client.user.tag}`); 
+// event listener for when the bot is ready and logged in
+client.once('Events.ClientReady', (readyClient) => { 
+    console.log(`Logged in as ${readyClient.user.tag}`); 
 });
 
+// logs in the bot using the token from the .env file
 client.login(TOKEN); 
 
 
